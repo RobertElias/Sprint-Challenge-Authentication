a119def70ad15fa38db885085ada66fb6486b9f0
robertelias <robert.elias@live.com>
2020-05-22 08:26:42 -0700

    updates

--- api/server.js
+++ api/server.js
@@ -1,22 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const dbConnection = require("../database/dbConfig.js");
 
 const server = express();
 
+const sessionConfig = {
+    name: "monster",
+    secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
+    resave: false,
+    saveUninitialized: process.env.SEND_COOKIES || true,
+    cookie: {
+        maxAge: 1000 * 60 * 60, // good for 1hr mins in ms
+        secure: process.env.USE_SECURE_COOKIES || false, // used over https only, set to true in production
+        httpOnly: true, // true means JS on the client cannot access the cooke
+    },
+    // the store property controls where the session is stored, by default it is in memory
+    // we're changing it to use the database through Knex
+    store: new KnexSessionStore({
+        knex: dbConnection,
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 1000 * 60 * 60, // will remove expired sessions every hour
+    }),
+};
+
 server.use(helmet());
-server.use(cors());
 server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use("/api/users", authenticator, usersRouter);
+server.use("/api/auth", authRouter);
+
 server.get("/", (req, res) => {
-    res.json({ api: "This API up" });
+    res.json({ api: "up" });
 });
-
 
 module.exports = server;
