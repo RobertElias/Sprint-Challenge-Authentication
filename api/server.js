016b1eb2a5dcadae50269692bf1468ef7ebadeaf
robertelias <robert.elias@live.com>
2020-05-22 08:19:43 -0700

    server.js updated

--- api/server.js
+++ api/server.js
@@ -14,5 +14,9 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+server.get("/", (req, res) => {
+    res.json({ api: "This API up" });
+});
+
 
 module.exports = server;
