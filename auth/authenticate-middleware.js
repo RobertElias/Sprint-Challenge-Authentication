c7dbb44330cb434f68fe2e38d674beb45d1f2b63
robertelias <robert.elias@live.com>
2020-05-22 08:27:09 -0700

    middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,23 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken')
+const secret = require('../api/secrets')
+
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, secret.jwtSecret, (err, decodeToken) => {
+            if (err) {
+                res.status(400).json({ message: 'cant pass' })
+            } else {
+                req.user = { username: decodeToken.username }
+                next()
+            }
+        })
+    } else {
+        res.status(400).json({ message: 'no token' })
+    }
+}
